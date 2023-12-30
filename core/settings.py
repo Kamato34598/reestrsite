84a2297fc6cba5ec0c0691d983099626e8158fda
Kamato34598 <155154382+Kamato34598@users.noreply.github.com>
2023-12-31 00:05:44 +0600

    Переопределение модели пользователя по умолчанию

--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'user.User'
+
 #django-compressor setings
 COMPRESS_ROOT = BASE_DIR / 'static'
 
