3e41d3f4518d8434831696daf165b15e0c14181d
Kamato34598 <155154382+Kamato34598@users.noreply.github.com>
2023-12-30 23:35:05 +0600

    locations: добавлено отображение в админ-панели модели областей, районов, городов user: добавлено отображение прокси моделей со связанными профилями моделей Patient и PatientChild

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Cities, Districts, Regions
 
-# Register your models here.
+@admin.register(Cities)
+class CitiesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'district', )
+    autocomplete_fields = ['district']
+    search_fields = ('name', 'district__name', 'district__region__name', )
+
+@admin.register(Districts)
+class DistrictsAdmin(admin.ModelAdmin):
+    list_display = ('custom_name', 'region')
+    def custom_name(self, obj):
+        return obj.__str__()
+    list_display_links = ('custom_name',)
+    ordering = ('region__name', 'name')
+    autocomplete_fields = ['region']
+    search_fields = ('name', 'region__name', )
+
+@admin.register(Regions)
+class RegionsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Patient, PatientProfile, PatientChildProfile, PatientChild
+from locations.models import Cities, Districts, Regions
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+
+class PatientProfileInline(admin.StackedInline):
+    model = PatientProfile
+    can_delete = False
+    verbose_name = _('Профиль пациента')
+    verbose_name_plural = _('Профили пациентов')
+    autocomplete_fields = ['city']
+
+
+class PatientAdmin(BaseUserAdmin):
+    inlines = [PatientProfileInline]
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'role')}),
+        # 'Patient Profile' в этом примере отображается после 'Personal info'
+        ('Patient Profile',
+         {'fields': ('iin', 'gender', 'city', 'height', 'disability', 'reabilitation', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_active','groups',)}),
+    )
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'city':
+            kwargs['queryset'] = Cities.objects.select_related('district__region').order_by('district__region__name',
+                                                                                            'district__name', 'name')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def add_view(self, *args, **kwargs):
+        self.inlines = []
+        return super(PatientAdmin, self).add_view(*args, **kwargs)
+
+    def change_view(self, *args, **kwargs):
+        self.inlines = [PatientProfileInline]
+        return super(PatientAdmin, self).change_view(*args, **kwargs)
+
+
+class PatientChildProfileInline(admin.StackedInline):
+    model = PatientChildProfile
+    can_delete = False
+    verbose_name = _('Профиль ребенка пациента')
+    verbose_name_plural = _('Профили детей пациентов')
+
+class PatientChildAdmin(BaseUserAdmin):
+    inlines = [PatientChildProfileInline]
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active',)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parent':
+            kwargs['queryset'] = Patient.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def add_view(self, *args, **kwargs):
+        self.inlines= []
+        return super(PatientChildAdmin, self).add_view(*args, **kwargs)
+
+    def change_view(self, *args, **kwargs):
+        self.inlines= [PatientChildProfileInline]
+        return super(PatientChildAdmin, self).change_view(*args, **kwargs)
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(PatientChild, PatientChildAdmin)
+
